92329ad98b8f94cb69d893c5d2f633b4d8bc2fa8
MrKevin574 <mrkevin574@gmail.com>
2022-07-15 14:38:59 -0400

    Adding cinzel font family

--- app/src/main/java/com/mrkevin574/memorygame/presentation/screens/startscreen/StartScreen.kt
+++ app/src/main/java/com/mrkevin574/memorygame/presentation/screens/startscreen/StartScreen.kt
@@ -2,9 +2,23 @@ package com.mrkevin574.memorygame.presentation.screens.startscreen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
 @Composable
-fun StartScreen(navController: NavController) {
-    Text("startScreen")
+fun StartScreen(navController: NavController? = null) {
+    TextStartScreen()
+}
+
+@Composable
+fun TextStartScreen() {
+    Text(
+        text = "Memory Game of MTG Arena"
+    )
+}
+
+@Preview
+@Composable
+fun PreviewStartScreen() {
+    StartScreen()
 }
--- app/src/main/java/com/mrkevin574/memorygame/ui/theme/Type.kt
+++ app/src/main/java/com/mrkevin574/memorygame/ui/theme/Type.kt
@@ -2,33 +2,26 @@ package com.mrkevin574.memorygame.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mrkevin574.memorygame.R
 
-// Set of Material typography styles to start with
+val cinzelFontFamily = FontFamily(
+    Font(R.raw.cinzel_black, FontWeight.Black),
+    Font(R.raw.cinzel_bold, FontWeight.Bold),
+    Font(R.raw.cinzel_extrabold, FontWeight.ExtraBold),
+    Font(R.raw.cinzel_medium, FontWeight.Medium),
+    Font(R.raw.cinzel_regular, FontWeight.Normal),
+    Font(R.raw.cinzel_semibold, FontWeight.SemiBold)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = cinzelFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
